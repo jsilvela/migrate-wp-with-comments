bcf3f86768369c6238bd093dd3079eca2c1ebba2
Jaime Silvela <jaime.silvela@gmail.com>
2022-03-27 17:06:55 +0200

    add comment threading

--- export-wp.go
+++ export-wp.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"text/template"
+	textTpl "text/template"
 	"time"
 )
 
@@ -16,6 +18,7 @@ const (
 	WORDPRESS_XML_FILE_PATH = "plazamoyuacom.wordpress.2022-03-07.000.xml" // "foo.xml"
 	OUTPUT_PATH             = "export3"
 	ORIGINAL_DOMAIN         = "https://plazamoyua.com"
+	FILES                   = "http://plazamoyua.files.wordpress.com/"
 )
 
 type rss struct {
@@ -60,17 +63,26 @@ type metaValue struct {
 }
 
 // comment represents a comment on the site, not an XML comment
+// the payload `Content` may contain embedded HTML, and is assumed to be
+// safe HTML
 type comment struct {
 	XMLName        xml.Name
-	AuthorName     string `xml:"comment_author"`
-	AuthorEmail    string `xml:"comment_author_email"`
-	AuthorURL      string `xml:"comment_author_url"`
-	Content        string `xml:"comment_content"`
-	Id             int    `xml:"comment_id"`
-	ParentId       int    `xml:"comment_parent"`
-	CommentDate    string `xml:"comment_date"`
-	CommentDateGMT string `xml:"comment_date_gmt"`
+	AuthorName     string        `xml:"comment_author"`
+	AuthorEmail    string        `xml:"comment_author_email"`
+	AuthorURL      string        `xml:"comment_author_url"`
+	Content        template.HTML `xml:"comment_content"`
+	Id             int           `xml:"comment_id"`
+	ParentId       int           `xml:"comment_parent"`
+	CommentDate    string        `xml:"comment_date"`
+	CommentDateGMT string        `xml:"comment_date_gmt"`
 	CreatedAt      time.Time
+}
+
+// commentThread represents a comment and its descendents
+type commentThread struct {
+	comment
+	Children     []commentThread
+	ChildrenHTML []template.HTML // this is just a convenience for rendering
 }
 
 func main() {
@@ -156,7 +168,7 @@ func main() {
 				if err != nil {
 					log.Fatalf("could not create file: %v", err)
 				}
-				err = commentsToHTML(f, it.Comments)
+				err = renderThreads(f, threadComments(it.Comments))
 				if err != nil {
 					log.Println("ayyyyssss", err)
 				}
@@ -173,6 +185,8 @@ func main() {
 	}
 }
 
+// toMarkdown adds a Hugo/jekyll front matter and displays a post/page as
+// markdown
 func (i item) toMarkdown(writer io.Writer) error {
 	var content string
 	for _, enc := range i.Encodeds {
@@ -181,7 +195,7 @@ func (i item) toMarkdown(writer io.Writer) error {
 		}
 	}
 
-	var tt template.Template
+	var tt textTpl.Template
 
 	d := struct {
 		Title   string
@@ -199,7 +213,7 @@ func (i item) toMarkdown(writer io.Writer) error {
 
 	t, err := tt.Parse(`
 ---
-title: "{{.Title | html}}"
+title: "{{.Title | html }}"
 date: "{{.PubDate}}"
 author: "{{.Author}}"
 slug: "{{.Slug}}"
@@ -211,6 +225,35 @@ slug: "{{.Slug}}"
 		return err
 	}
 	return t.Execute(writer, d)
+}
+
+// threadComments takes a list of comments, sorts through their
+// family tree using the `ParentID` and `ID` attributes, and converts them
+// to a list of threads where each node has its Children threads
+//
+// NOTE: assumes the comments form a tree
+func threadComments(comments []comment) []commentThread {
+	roots := make([]commentThread, 0, len(comments))
+	for _, c := range comments {
+		if c.ParentId == 0 {
+			roots = append(roots, threadCommentLevel(c, comments))
+		}
+	}
+
+	return roots
+}
+
+func threadCommentLevel(node comment, comments []comment) commentThread {
+	var threads []commentThread
+	for _, c := range comments {
+		if c.ParentId == node.Id {
+			threads = append(threads, threadCommentLevel(c, comments))
+		}
+	}
+	var re commentThread
+	re.comment = node
+	re.Children = threads
+	return re
 }
 
 func commentsToHTML(writer io.Writer, comments []comment) error {
@@ -235,6 +278,84 @@ func commentsToHTML(writer io.Writer, comments []comment) error {
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+// threadToHTML renders a single thread as HTML, starting a new
+// sub ordered-list fore each generation
+// NOTE: Markdown could accomodate this too, but being whitespace-sensitive,
+// this makes it an inconvenient choice. HTML proves the better format for code-gen
+func threadToHTML(thread commentThread) (template.HTML, error) {
+	t, err := template.New("tpl").Parse(`
+	<li>
+	<div class="comment">
+		<span class="author">{{.AuthorName}}</span>
+		<span class="date">{{.CommentDate}}</span>
+		<div>
+			{{.Content}}
+		</div>
+		{{ with .ChildrenHTML}}
+		<div class="children">
+			<ul>
+			{{ range . }}
+				{{ . }}
+			{{ end }}
+			</ul>
+		</div>
+		{{ end }}
+	</div>
+	</li>
+`)
+	if err != nil {
+		log.Fatalf("bad template: %v", err)
+	}
+
+	if len(thread.Children) == 0 {
+		buffer := bytes.Buffer{}
+		err = t.Execute(&buffer, thread)
+		if err != nil {
+			return "", err
+		}
+		return template.HTML(buffer.String()), nil
+	}
+
+	for _, child := range thread.Children {
+		ht, err := threadToHTML(child)
+		if err != nil {
+			return "", err
+		}
+		thread.ChildrenHTML = append(thread.ChildrenHTML, ht)
+	}
+	buffer := bytes.Buffer{}
+	err = t.Execute(&buffer, thread)
+	if err != nil {
+		return "", err
+	}
+
+	return template.HTML(buffer.String()), err
+}
+
+// renderThreads goes overa all the comment threads and renders them to the
+// appropriate writer/file/buffer
+func renderThreads(writer io.Writer, comments []commentThread) error {
+	_, err := writer.Write([]byte("<div class=\"comments\"><ul>\n"))
+	if err != nil {
+		return err
+	}
+	for _, c := range comments {
+		ht, err := threadToHTML(c)
+		if err != nil {
+			return err
+		}
+		_, err = writer.Write([]byte(ht))
+		if err != nil {
+			return err
+		}
+	}
+	_, err = writer.Write([]byte("</ul></div>\n"))
+	if err != nil {
+		return err
 	}
 	return nil
 }
